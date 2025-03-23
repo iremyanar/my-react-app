9e44a99ec541fb0e5f1c96fa6c50448f9f16216b
iremyanar <46534923+iremyanar@users.noreply.github.com>
2025-03-23 16:45:57 +0300

    Yeni değişiklikler

--- src/components/NewProduct/ProductForm.jsx
+++ src/components/NewProduct/ProductForm.jsx
@@ -2,26 +2,46 @@ import { useState } from "react";
 import "./ProductForm.css";
 
 const ProductForm = () => {
-  const [productName, setProductName] = useState("");
-  const [productPrice, setProductPrice] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
+  // const [productName, setProductName] = useState("");
+  // const [productPrice, setProductPrice] = useState("");
+  // const [imageUrl, setImageUrl] = useState("");
+
+  const [productData, setProductData] = useState({
+    productName: "",
+    productPrice: "",
+    imageUrl: "",
+  });
 
   const titleChangeHandler = (event) => {
-    setProductName(event.target.value);
+    // setProductName(event.target.value);
+    setProductData({
+      ...productData,
+      productName: event.target.value,
+    });
   };
 
   const priceChangeHandler = (event) => {
-    setProductPrice(event.target.value);
+    // setProductPrice(event.target.value);
+    setProductData({
+      ...productData,
+      productPrice: event.target.value,
+    });
   };
 
   const imageChangeHandler = (event) => {
-    setImageUrl(event.target.value);
+    // setImageUrl(event.target.value);
+    setProductData({
+      ...productData,
+     imageUrl: event.target.value,
+    });
   };
+
+  console.log(productData);
 
   return (
     <form className="product-form">
       <div className="product-form-input">
-        <label>Ürün Adı: {productName}</label>
+        <label>Ürün Adı: {productData.productName}</label>
         <input
           type="text"
           placeholder="Ürün Adı Giriniz..."
@@ -29,7 +49,7 @@ const ProductForm = () => {
         />
       </div>
       <div className="product-form-input">
-        <label>Ürün Fiyatı: {productPrice}</label>
+        <label>Ürün Fiyatı: {productData.productPrice}</label>
         <input
           type="number"
           placeholder="Ürün Fiyatı Giriniz..."
@@ -37,7 +57,7 @@ const ProductForm = () => {
         />
       </div>
       <div className="product-form-input">
-        <label>Ürün Görseli {imageUrl}</label>
+        <label>Ürün Görseli {productData.imageUrl}</label>
         <input type="text" placeholder="Ürün Görseli Giriniz..." onChange={imageChangeHandler} />
       </div>
       <button className="product-form-button">Ürün Ekle</button>
